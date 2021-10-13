7c00b726c94391283839b34d3f017d0d00dcd768
Becki Floyd <beckifloyd@gmail.com>
2021-10-13 15:57:27 +0100

    Added rel link

--- portfolio-tailwind-react/src/components/helpers/navbar.js
+++ portfolio-tailwind-react/src/components/helpers/navbar.js
@@ -2,9 +2,9 @@ function Navbar() {
   return (
     <div className="text-white text-xl lg:text-2xl flex justify-end items-center text-m w-auto ml-5 mr-5 ">
     
-      <a className="ml-5 mr-5 hover:text-secondary" target="_blank" href="http://www.github.com/nass84/">GitHub</a>
-      <a className="mr-5 hover:text-secondary" target="_blank" href="mailto:beckifloyd@gmail.com/">Email</a>
-      <a className="hover:text-secondary" target="_blank" href="http://www.linkedin.com/in/nass84/">LinkedIn</a>
+      <a className="ml-5 mr-5 hover:text-secondary" target="_blank" href="http://www.github.com/nass84/" rel="noreferrer">GitHub</a>
+      <a className="mr-5 hover:text-secondary" target="_blank" rel="noreferrer" href="mailto:beckifloyd@gmail.com/">Email</a>
+      <a className="hover:text-secondary" target="_blank" rel="noreferrer" href="http://www.linkedin.com/in/nass84/">LinkedIn</a>
     
     </div>
   );
