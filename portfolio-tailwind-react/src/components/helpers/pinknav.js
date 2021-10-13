30844618e6ba859b3456d1a004fd8a797301b766
Becki Floyd <beckifloyd@gmail.com>
2021-10-13 16:38:23 +0100

    ADDED S TO HTTP

--- portfolio-tailwind-react/src/components/helpers/pinknav.js
+++ portfolio-tailwind-react/src/components/helpers/pinknav.js
@@ -3,9 +3,9 @@ function PinkNav() {
     <div>
     <div className="text-primary text-xl lg:text-2xl flex justify-end items-center text-m w-auto ml-5 mr-6 font-bold ">
     
-      <a className="ml-5 mr-5 hover:text-secondary hover:font-bold" href="http://www.github.com/nass84/" rel="noreferrer" target="_blank">GitHub</a>
+      <a className="ml-5 mr-5 hover:text-secondary hover:font-bold" href="https://www.github.com/nass84/" rel="noreferrer" target="_blank">GitHub</a>
       <a className="mr-5 hover:text-secondary hover:font-bold" href="mailto:beckifloyd@gmail.com/" rel="noreferrer" target="_blank">Email</a>
-      <a className="hover:text-secondary hover:font-bold" href="http://www.linkedin.com/in/nass84/" rel="noreferrer" target="_blank">LinkedIn</a>
+      <a className="hover:text-secondary hover:font-bold" href="https://www.linkedin.com/in/nass84/" rel="noreferrer" target="_blank">LinkedIn</a>
     
     </div>
     <div className="bg-primary h-0.5 mt-1 mb-10 "></div>
